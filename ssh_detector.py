bb5905672d81102f70c643dc961048ab22b28d73
MasonKadedra <162503087+MasonKadedra@users.noreply.github.com>
2026-03-31 08:24:28 -0500

    Refactor regex pattern and improve code formatting

--- ssh_detector.py
+++ ssh_detector.py
@@ -4,37 +4,42 @@ import re
 from datetime import datetime, timedelta
 
 Log_File = "/var/log/auth.log"
-pattern = re.compile(r'(?P<timestamp>\w{3}\s+\d+\s[\d:]+).*Failed password for (invalid user )?(?P<user>\S+) from (?P<ip>\d+\.\d+\.\d+\.\d+)')
+
+pattern = re.compile(
+    r'(?P<timestamp>\w{3}\s+\d+\s[\d:]+).*Failed password for (invalid user )?(?P<user>\S+) from (?P<ip>\d+\.\d+\.\d+\.\d+)'
+)
 
 def parse_timestamp(ts):
     return datetime.strptime(ts, "%b %d %H:%M:%S").replace(year=datetime.now().year)
 
 def detect_bruteforceAttack():
-    
     failed_attempt = {}
     alerts = []
+
     with open(Log_File, "r") as f:
         for line in f:
-            
             match = pattern.search(line)
             if match:
-                log_time = parse_timestamp(match.group ("timestamp"))
+                log_time = parse_timestamp(match.group("timestamp"))
                 ip = match.group("ip")
                 user = match.group("user")
-                
+
                 if ip not in failed_attempt:
                     failed_attempt[ip] = []
-                    
+
                 failed_attempt[ip].append((log_time, user))
-                    
-                recent_attempts = [attempt for attempt in failed_attempt[ip]
-                                        if log_time - attempt[0] <= timedelta(minutes = 2)]
-                    
-                    if len(recent_attempts) >= 5:
-                        alerts.append({
-                            "ip": ip,
-                            "user": user,
-                            "count": len(recent_attempts),
-                            "time": log_time.strftime("%Y-%m-%d %H:%M:%S")
-                            })
+
+                recent_attempts = [
+                    attempt for attempt in failed_attempt[ip]
+                    if log_time - attempt[0] <= timedelta(minutes=2)
+                ]
+
+                if len(recent_attempts) >= 5:
+                    alerts.append({
+                        "ip": ip,
+                        "user": user,
+                        "count": len(recent_attempts),
+                        "time": log_time.strftime("%Y-%m-%d %H:%M:%S")
+                    })
+
     return alerts
