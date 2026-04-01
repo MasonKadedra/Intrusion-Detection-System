085f01fe456f9a42f388275d7350c65338d555f7
Nutzyy <166946835+Nutzyy@users.noreply.github.com>
2026-03-31 19:04:22 -0500

    alert.py

--- alert.py
+++ alert.py
@@ -1,4 +1,15 @@
-import os
+# Brandon Blake
+#2202851
+
+import subprocess
+
+# PURPOSE: Sends a desktop notification when a threat is detected
 
 def send_alert(message):
-    os.system(f'notify-send "HIDS Alert" "{message}"')
+    try:
+        # notify-send is a Linux tool used for pop-up notifications
+        subprocess.run(["notify-send", "HIDS Alert", message])
+    except:
+        # fallback in case notification fails
+        print("ALERT:", message)
+
